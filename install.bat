88e7bf7cee7f92bc26a1db6c43c6b6a9f9d45ed4
kairoooo-dev <kairoooo-dev@users.noreply.github.com>
2026-09-03 22:31:19 +0800

    Add Windows batch installer

---
+++ install.bat
@@ -0,0 +1,4 @@
+@echo off
+title KRUX Executor Installer
+powershell -ExecutionPolicy Bypass -Command "iex (irm https://raw.githubusercontent.com/kairoooo-dev/krux-executor/master/install.ps1)"
+pause
