b540c9b043f008f5e5fcc5cd944088b16a9e62e7
kairoooo-dev <kairoooo-dev@users.noreply.github.com>
2026-09-04 02:28:16 +0800

    fix: KRUX Solara - resource manifest namespace crash fix

--- KRUX-Solara/Executor/Properties/Resources.Designer.cs
+++ KRUX-Solara/Executor/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace KRUX.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Executor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KRUX.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
