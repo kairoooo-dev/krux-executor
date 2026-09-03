ba53026e573d32441fe7e981defda5e96fc20c2f
kairoooo-dev <kairoooo-dev@users.noreply.github.com>
2026-09-04 02:20:32 +0800

    feat: KRUX Solara rebrand - full source + build output (WebSocket executor)

---
+++ KRUX-Solara/Executor/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRUX
+{
+    static class Program
+    {
+        public static KRUXForm exec;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+       {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            exec = new KRUXForm();
+            Application.Run(exec);
+        }
+    }
+}
